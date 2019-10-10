f01fe6ff56b68117f408bdf31421b7ff826c75f5
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-10 12:48:31 +0200

    Add 5 - 10 exercises

---
+++ Exercises from videos.py
@@ -0,0 +1,37 @@
+print('Hello world')
+print()
+print('Did you see that blank line?')
+print('Blank line \nin the middle of string') 
+# ctrl + k + c  ----> comment line
+# ctrl + k + u -----> uncomment line
+
+first_name = 'Bartosz'
+last_name = 'Maleta'
+print(first_name + last_name)
+print('Hello ' + first_name + ' ' + last_name)
+
+sentence = 'The dog is named Sammy'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
+
+
+# first_name2 = input('What is your first name? ')  ----> commented, because wanted to continue with more examples
+# last_name2 = input('What is your last name? ')
+# print('Hello ' + first_name2.capitalize() + ' ' + last_name2.capitalize())
+# ___________________________________________
+
+
+first_name3 = 'Bartosz'
+last_name3 = 'Maleta'
+
+# first_name3 = input('Please enter your first name')
+# last_name3 = input('Please enter your last name')
+
+print(first_name3 + last_name3)
+print('Hello, ' + first_name3 + ' ' + last_name3)
+
+print(last_name3.upper())
+
+
