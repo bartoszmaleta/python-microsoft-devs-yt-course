56c1cbe3ab0e7992033b9af619bd95a9df1213aa
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-10 17:11:40 +0200

    Added 14th video exercise str to int

---
+++ 14th video demo: numbers.py
@@ -0,0 +1,16 @@
+pi = 3.14159
+print(pi)
+
+# Commented just so i could continue with excersies.
+# Can uncomment!
+
+# first_number = input('Enter a number: ')
+# second_number = input('Enter another number: ')
+# print(int(first_number) ** int(second_number))
+
+# print(float(first_number) ** float(second_number))
+
+
+days_in_Februray = 28
+print(str(days_in_Februray) + ' total days in February')
+
