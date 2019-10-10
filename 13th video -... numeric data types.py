4b8567418b286e48fedf156744d7677629f5d997
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-10 13:32:22 +0200

    Added changing types: int, str, float

---
+++ 13th video -... numeric data types.py
@@ -0,0 +1,20 @@
+pi = 3.14159
+print(pi)
+
+first_number = 5
+second_number = 2
+print(first_number ** second_number)
+print(first_number + second_number)
+
+
+days_in_feb = 28
+print(str(days_in_feb) + ' days in February')
+
+first_number2 = '3'
+second_number2 = '4'
+
+print(first_number2 + second_number2)
+# output 34
+
+print(int(first_number2) + int(second_number2))
+print(float(first_number2) + float(second_number2))
