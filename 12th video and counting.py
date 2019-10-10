fd1ecb92720c6bf7cf321a93d86803c6b78dfc31
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-10 13:19:38 +0200

    Added: output f'Hello, {first...

---
+++ 12th video and counting.py
@@ -0,0 +1,14 @@
+first_name = 'Bartosz'
+last_name = 'Maleta'
+
+# output = 'Hello, ' + first_name + ' ' + last_name
+
+# output = 'Hello, {} {}'.format(first_name, last_name)
+
+# output = 'Hello, {0} {1}'.format(first_name, last_name)
+
+
+# Preferred, recommended
+output = f'Hello, {first_name} {last_name}'
+
+print(output)
