39deb59da65662a18dfce072c136b5e452120fe2
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-11 22:15:10 +0200

    33 - commented not runable lines, 34th - blank

--- 33rd video Modules and packages.py
+++ 33rd video Modules and packages.py
@@ -1,16 +1,16 @@
-#this file won't work, because imports are wrong
+# this file won't work, because imports are wrong
 
-# import module as namespace
-import helpes
-helpers.display('Not a warning')
+# # import module as namespace:
+# import helpes
+# helpers.display('Not a warning')
 
-# import all into current namespace
-from helpers import *
-display('Not a warning')
+# # import all into current namespace:
+# from helpers import *
+# display('Not a warning')
 
-# import specific items into current namespace
-from helpers import display
-display('Not a warning')
+# # import specific items into current namespace:
+# from helpers import display
+# display('Not a warning')
 
 
 def display(message, is_warning=False):
